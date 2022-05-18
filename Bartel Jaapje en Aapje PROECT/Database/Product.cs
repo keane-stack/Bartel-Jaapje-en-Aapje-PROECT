b3c1d9ff9f1a1726210a5b89f3b32bbde96cc019
Bart Tabaksblat <110821@st-maartenscollege.nl>
2022-05-18 10:42:01 +0200

    Begin blokjes

---
+++ Bartel Jaapje en Aapje PROECT/Database/Product.cs
@@ -0,0 +1,12 @@
+﻿namespace Bartel_Jaapje_en_Aapje_PROECT.Database
+{
+    public class Product
+    {
+       public int Id { get; set; }
+        public string Name { get; set; }
+        public string Prijs { get; set; }
+        public int Beschikbaarheid { get; set; }
+
+
+    }
+}

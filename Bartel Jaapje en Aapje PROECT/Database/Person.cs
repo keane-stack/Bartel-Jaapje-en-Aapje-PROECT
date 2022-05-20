1312387a935f6dc7b95de0717652ac5b78285356
Keane van der Moolen <110735@st-maartenscollege.nl>
2022-05-20 12:37:34 +0200

    contact pagina werkt eindelijk

---
+++ Bartel Jaapje en Aapje PROECT/Database/Person.cs
@@ -0,0 +1,17 @@
+﻿namespace Bartel_Jaapje_en_Aapje_PROECT.Database
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Email { get; set; }
+
+        public string Phone { get; set; }
+
+        public string Address { get; set; }
+
+        public string Description { get; set; }
+    }
+}
